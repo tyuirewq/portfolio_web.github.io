2d0ea2b9df7221e670f150dec8c9bb515977c4e0
tyuirewq <86822476+tyuirewq@users.noreply.github.com>
2022-08-08 21:30:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// typing Animation
+var typed = new Typed(".typing",{
+    strings:["","Web Developer","App Developer","Java Programmer","Database Analysis"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+// Aside
+// const nav = document.querySelector(".nav"),
+//         navList = nav.querySelectorAll("li"),
+//         totalNavList = navList.length;
+//         for (let i = 0; i < totalNavList.length; i++) {
+//             console.log(navList[i])
+//         }
