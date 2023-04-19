b8bc4925e9ef3342fed98188c3d403e3c664bc09
Hariom Kaushal <86822476+tyuirewq@users.noreply.github.com>
2023-04-19 10:39:28 +0530

    Update style-switcher.js

--- style-switcher.js
+++ style-switcher.js
@@ -29,7 +29,7 @@ dayNight.addEventListener("click", () => {
     document.body.classList.toggle("dark")
 })
 window.addEventListener("load",() =>{
-    if(document.body.classList.contains("day-night")){
+    if(document.body.classList.contains("dark")){
         dayNight.querySelector("i").classList.add("fa-sun");
     }
     else{
