f003e7350931c08b76f6a62756569f390770a2aa
Hariom Kaushal <86822476+tyuirewq@users.noreply.github.com>
2023-04-19 10:34:33 +0530

    Update style-switcher.js

--- style-switcher.js
+++ style-switcher.js
@@ -22,14 +22,14 @@ function setActiveStyle(color) {
     })
 }
 // theme light and dark mode
-const dayNight = document.querySelector(".day-night");
+const dayNight = document.querySelector(".dark");
 dayNight.addEventListener("click", () => {
-    dayNight.querySelector("i").classList.toggle("fa-sun");
     dayNight.querySelector("i").classList.toggle("fa-moon");
-    document.body.classList.toggle("dark")
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    document.body.classList.toggle("day-night")
 })
 window.addEventListener("load",() =>{
-    if(document.body.classList.contains("dark")){
+    if(document.body.classList.contains("day-night")){
         dayNight.querySelector("i").classList.add("fa-sun");
     }
     else{
